c2510cbf3ebfe3151c52f24156c8869924a6bcb8
Abraham Putra Berman Simbolom <abrahamputrabs@gmail.com>
2025-06-03 14:34:08 +0700

    Memproses data string sebagai JSON

---
+++ app/src/main/java/com/abrahamputra0058/ourbday/model/BirthdayUser.kt
@@ -0,0 +1,7 @@
+package com.abrahamputra0058.ourbday.model
+
+data class BirthdayUser(
+    val nama: String,
+    val tanggalLahir: String,
+    val imageId: String
+)
--- app/src/main/java/com/abrahamputra0058/ourbday/network/BirthdayUserApiService.kt
+++ app/src/main/java/com/abrahamputra0058/ourbday/network/BirthdayUserApiService.kt
@@ -1,7 +1,10 @@
 package com.abrahamputra0058.ourbday.network
 
+import com.abrahamputra0058.ourbday.model.BirthdayUser
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 private val BASE_URL = "https://mpa0097dcdfc840f8471.free.beeceptor.com/"
@@ -9,14 +12,18 @@ private val BASE_URL = "https://mpa0097dcdfc840f8471.free.beeceptor.com/"
 //GET - /birthday-user
 //https://mpa0097dcdfc840f8471.free.beeceptor.com/birthday-user.json
 
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
 private val retrofit = Retrofit.Builder()
-    .addConverterFactory(ScalarsConverterFactory.create())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
     .build()
 
 interface BirthdayUserApiService {
     @GET("birthday-user.json")
-    suspend fun getBirthdayUser(): String
+    suspend fun getBirthdayUser(): List<BirthdayUser>
 }
 
 object BirthdayUserApi {
